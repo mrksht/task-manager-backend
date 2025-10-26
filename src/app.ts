a880295dd2c34fcf929f813480111cf9b41e7dfb
Rakshit <rakshit@Rakshits-MacBook-Pro.local>
2025-10-26 13:52:52 +0530

    learning agentic AI

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import todoRoutes from "./routes/todoRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://task-manager-omega-tawny.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
